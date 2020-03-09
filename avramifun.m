6079b8b7123e16b0dbd60405988bf8d59b50fd3c
AarnePohjonen <61888191+AarnePohjonen@users.noreply.github.com>
2020-03-09 18:13:20 +0200

    Add files via upload

---
+++ avramifun.m
@@ -0,0 +1,11 @@
+function [outputdata] = avramifun(x,inputxdata)
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+outputdata=zeros(size(inputxdata));
+for i=1:length(inputxdata)
+    if(inputxdata(i))>x(3)
+        outputdata(i) = (1-exp(-real(x(1))*(inputxdata(i)-real(x(3)))^(real(x(2)))));
+    end
+end
+end
+
